ce3a54755bc9d4c2c9b95556550916ef36fbf139
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-05-15 17:55:58 -0300

    Dividiendo los primero ejercicios de TP 4

--- tp_4/ej_3_4_5.hs
+++ tp_4/ej_3_4_5.hs
@@ -1,47 +1,4 @@
-data Color = Rojo | Amarillo | Azul | Verde
-    deriving (Show, Eq)
-data Forma = Triangulo | Cuadrado | Rombo | Circulo
-    deriving (Show, Eq)
-
-type Figura = (Forma, Color, Int)
-
-{-
-########################### 1 ###########################
-A cada figura se pueden asociar diferentes predicados que indiquen el color y la forma. Implementa en Haskell las funciones correspondientes a los predicados azul, amarillo, verde, circulo,
-rombo, cuadrado y triangulo
--}
--- Es Color
-rojo :: Figura ->Bool
-rojo (f,c,t) = c == Rojo
-
-azul :: Figura ->Bool
-azul (f,c,t) = c == Azul
-
-amarillo :: Figura ->Bool
-amarillo (f,c,t) = c == Amarillo
-
-verde :: Figura ->Bool
-verde (f,c,t) = c == Verde
-
--- Es Forma
-circulo :: Figura ->Bool
-circulo (f,c,t) = f == Circulo
-
-triangulo :: Figura ->Bool
-triangulo (f,c,t) = f == Triangulo
-
-cuadrado :: Figura ->Bool
-cuadrado (f,c,t) = f == Cuadrado
-
-rombo :: Figura ->Bool
-rombo (f,c,t) = f == Rombo
-
-{-
-########################### 2 ###########################
-Definı la funcion tam :: Figura -> Int, que dada una figura devuelve su tamaño
--}
-tam :: Figura -> Int
-tam (f,c,t) = t
+import Ej_1_2
 
 {-
 ########################### 3 y 4 ###########################
