63f420578216bb685b1216aacd67e8d4ca479d57
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-08-20 22:50:02 -0300

    Lab 1 - Ej 2

--- Alg_y_Est_Datos_1/lab1.hs
+++ Alg_y_Est_Datos_1/lab1.hs
@@ -14,3 +14,29 @@ esVocal c = c == 'a' || c== 'e' || c== 'i' || c== 'o' || c== 'u'
 -- d
 valorAbsoluto :: Int -> Int
 valorAbsoluto x = if x >= 0 then x else (-x)
+
+-- ######################## 2 ########################
+
+-- a
+paratodo :: [Bool] -> Bool
+paratodo [] = True
+paratodo (x:xs) = x && paratodo xs
+
+-- b
+sumatoria :: [Int] -> Int
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+-- c
+productoria :: [Int] -> Int
+productoria [] = 1
+productoria (x:xs) = x * productoria xs
+
+-- d
+factorial :: Int -> Int
+factorial 0 = 1
+factorial x = x * factorial (x - 1)
+
+-- e
+promedio :: [Int] -> Int
+promedio xs = div (sumatoria xs) (length xs)
