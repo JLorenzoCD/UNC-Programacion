86adc0655b12c7dfa127b7b0c0334d32f7d56926
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-10-03 09:47:56 -0300

    Lab 3 - Ej 5 - Correccion y añadiendo ejecucion del ejercicio

--- Alg_y_Est_Datos_1/lab3/ej5.c
+++ Alg_y_Est_Datos_1/lab3/ej5.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 
 void apartado_a(void);
+void ejercicio_h(void);
+void ejercicio_i(void);
 
 void apartado_b(void);
 void punto_1(void);
@@ -10,8 +12,8 @@ void punto_2(void);
 
 int main(void)
 {
-    // printf("-------------------- Apartado A --------------------\n");
-    // apartado_a();
+    printf("-------------------- Apartado A --------------------\n");
+    apartado_a();
 
     printf("\n\n-------------------- Apartado B --------------------\n");
     apartado_b();
@@ -20,6 +22,14 @@ int main(void)
 }
 
 void apartado_a(void){
+    printf("-------------------- Ejercicio 1.h --------------------\n");
+    ejercicio_h();
+
+    printf("\n\n-------------------- Ejercicio 1.i --------------------\n");
+    ejercicio_i();
+}
+
+void ejercicio_h(void){
     int i;
 
     printf("############### Estado σ0 ###############\n");
@@ -37,10 +47,27 @@ void apartado_a(void){
     printf("Termina el programa con i = %i\n", i);
 }
 
+void ejercicio_i(void){
+    int i;
+
+    printf("############### Estado σ0 ###############\n");
+    printf("-- Asignacion de variables\n");
+    printf("Valor de i: ");
+    scanf("%i", &i);
+
+    while (i != 0)
+    {
+        printf("Valor de i previo a la resta: %i\n", i);
+        i = 0;
+        printf("Valor de i posterior a la resta: %i\n\n", i);
+    }
+    
+    printf("Termina el programa con i = %i\n", i);
+}
 
 void apartado_b(void){
-    // printf("========= Punto 1 =========\n");
-    // punto_1();
+    printf("========= Punto 1 =========\n");
+    punto_1();
     printf("\n\n========= Punto 2 =========\n");
     punto_2();
 }
@@ -56,6 +83,9 @@ void punto_1(void){
     printf("Valor de y: ");
     scanf("%i", &y);
 
+    printf("Valor de i: ");
+    scanf("%i", &i);
+
     i = 0;
     while (x >= y)
     {
@@ -69,13 +99,20 @@ void punto_1(void){
 }
 
 void punto_2(void){
-    int x, i;
+    int x, i, temp;
     bool res;
 
     printf("############### Estado σ0 ###############\n");
     printf("-- Asignacion de variables\n");
     printf("Valor de x: ");
     scanf("%i", &x);
+
+    printf("Valor de i: ");
+    scanf("%i", &i);
+
+    printf("Valor de res: ");
+    scanf("%i", &temp);
+    res = temp;
 
     res = true;
     i = 2;
@@ -89,3 +126,14 @@ void punto_2(void){
         i += 1;
     }
 }
+
+/*
+Ejecucion del ejercicio
+-- a
+Ejecucion h    |[σ0 : (i => 4)]|     =>   |[σ : (i => 0)]|
+Ejecucion i    |[σ0 : (i => 400)]|   =>   |[σ : (i => 0)]|
+
+-- b
+Ejecucion 1    |[σ0 : (x => 13,   y => 3,  i => 0)]|        =>   |[σ : (x => 1,   y => 3,  i => 4)]|
+Ejecucion 2    |[σ0 : (x => 5,   res => False,  i => 0)]|   =>   |[σ : (x => 5,   res => True,  i => 4)]|
+*/
