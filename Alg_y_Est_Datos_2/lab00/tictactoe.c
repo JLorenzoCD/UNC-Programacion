97dcc0072f36198ef215b206dac2e359f237327b
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-14 16:49:06 -0300

    Lab 00 tictactoe - Modificando formato y definiendo BOARD_LEN

--- Alg_y_Est_Datos_2/lab00/tictactoe.c
+++ Alg_y_Est_Datos_2/lab00/tictactoe.c
@@ -4,7 +4,8 @@
 
 #include <assert.h>  /* assert() */
 
-#define CELL_MAX (3 * 3 - 1)
+#define BOARD_LEN 3
+#define CELL_MAX (BOARD_LEN * BOARD_LEN - 1)
 
 #define CELL_FREE '-'
 #define PLAYER_1 'X'
@@ -17,24 +18,25 @@ void print_sep(int length) {
 
 }
 
-void print_board(char board[3][3]) {
+void print_board(char board[BOARD_LEN][BOARD_LEN]) {
     int cell = 0;
 
-    print_sep(3);
-    for (int row = 0; row < 3; ++row) {
-        for (int column = 0; column < 3; ++column) {
+    print_sep(BOARD_LEN);
+    for (unsigned int row = 0; row < BOARD_LEN; ++row) {
+        for (unsigned int column = 0; column < BOARD_LEN; ++column) {
             printf("\t | %d: %c ", cell, board[row][column]);
             ++cell;
         }
+
         printf("\t | \n");
-        print_sep(3);
+        print_sep(BOARD_LEN);
     }
 }
 
-char get_winner(char board[3][3]) {
+char get_winner(char board[BOARD_LEN][BOARD_LEN]) {
     char winner = CELL_FREE;
 
-    for (unsigned int i = 0; i < 3 && winner == CELL_FREE; i++) {
+    for (unsigned int i = 0; i < BOARD_LEN && winner == CELL_FREE; i++) {
         // Revisando si se hizo tic-tac-toe en las filas
         if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
             winner = board[i][0];
@@ -57,16 +59,15 @@ char get_winner(char board[3][3]) {
     return winner;
 }
 
-bool has_free_cell(char board[3][3]) {
+bool has_free_cell(char board[BOARD_LEN][BOARD_LEN]) {
     bool free_cell = false;
 
-    for (unsigned int i = 0; i < 3 && !free_cell; i++) {
-        for (unsigned int j = 0; j < 3 && !free_cell; j++) {
-            if (board[i][j] == CELL_FREE) {
+    for (unsigned int row = 0; row < BOARD_LEN && !free_cell; row++) {
+        for (unsigned int column = 0; column < BOARD_LEN && !free_cell; column++) {
+            if (board[row][column] == CELL_FREE) {
                 free_cell = true;
             }
         }
-
     }
 
     return free_cell;
@@ -75,7 +76,7 @@ bool has_free_cell(char board[3][3]) {
 int main(void) {
     printf("TicTacToe [COMPLETO]\n\n");
 
-    char board[3][3] = {
+    char board[BOARD_LEN][BOARD_LEN] = {
         { CELL_FREE, CELL_FREE, CELL_FREE },
         { CELL_FREE, CELL_FREE, CELL_FREE },
         { CELL_FREE, CELL_FREE, CELL_FREE }
@@ -84,20 +85,24 @@ int main(void) {
     char turn = PLAYER_1;
     char winner = CELL_FREE;
     int cell = 0;
+
     while (winner == CELL_FREE && has_free_cell(board)) {
         print_board(board);
-        printf("\nTurno %c - Elija posición (número del 0 al %d): ", turn,
-            CELL_MAX);
+        printf("\nTurno %c - Elija posición (número del 0 al %d): ", turn, CELL_MAX);
         int scanf_result = scanf("%d", &cell);
+
         if (scanf_result <= 0) {
             printf("Error al leer un número desde teclado\n");
             exit(EXIT_FAILURE);
         }
+
         if (cell >= 0 && cell <= CELL_MAX) {
-            int row = cell / 3;
-            int colum = cell % 3;
+            int row = cell / BOARD_LEN;
+            int colum = cell % BOARD_LEN;
+
             if (board[row][colum] == CELL_FREE) {
                 board[row][colum] = turn;
+
                 turn = turn == PLAYER_1 ? PLAYER_2 : PLAYER_1;
                 winner = get_winner(board);
             }
@@ -110,11 +115,13 @@ int main(void) {
         }
     }
     print_board(board);
+
     if (winner == CELL_FREE) {
         printf("Empate!\n");
     }
     else {
         printf("Ganó %c\n", winner);
     }
+
     return 0;
 }
