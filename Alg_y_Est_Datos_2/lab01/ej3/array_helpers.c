3030ffd7edf4c7992bf23a328fcaa180cf2bbc8e
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-16 12:04:57 -0300

    Lab01 - Ej3

---
+++ Alg_y_Est_Datos_2/lab01/ej3/array_helpers.c
@@ -0,0 +1,35 @@
+#include "array_helpers.h"
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath) {
+    FILE *file = fopen(filepath, "r"); // Open mode READONLY
+
+    unsigned int length;
+    fscanf(file, "%u", &length);
+
+    if (max_size < length) {
+        printf("Only arrays with a maximum length of '%u' are allowed", max_size);
+        exit(EXIT_FAILURE);
+    }
+
+    int x;
+    for (unsigned int i = 0; i < length; i++) {
+        fscanf(file, "%d", &x);
+        array[i] = x;
+    }
+
+    fclose(file);
+
+    return length;
+}
+
+void array_dump(int a[], unsigned int length) {
+    printf("\n[");
+    for (unsigned int i = 0; i < length; i++) {
+        printf("%d", a[i]);
+
+        if (i + 1 < length) {
+            printf(", ");
+        }
+    }
+    printf("]\n\n");
+}
---
+++ Alg_y_Est_Datos_2/lab01/ej3/array_helpers.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath);
+void array_dump(int a[], unsigned int length);
