f0a75dccbbd0345c07470555fa100f6176141f50
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-16 22:13:17 -0300

    Lab01 - Ej5 - Removing repeated code

--- Alg_y_Est_Datos_2/lab01/ej5/mybool.h
+++ Alg_y_Est_Datos_2/lab01/ej5/mybool.h
@@ -1,7 +1,6 @@
 #ifndef MY_BOOL_MODULE
 #define MY_BOOL_MODULE
 #pragma once // En toeoria solo con esta linea al incicio bastaria, pero al aprecer no es standar a si que se utiliza el #ifndef ....
-#pragma once
 
 #define true 1
 #define false 0
