1995e50a1bf64839dbe7abcbabf9e2bccddf9608
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-16 22:29:14 -0300

    Lab01 - Ej6

---
+++ Alg_y_Est_Datos_2/lab01/ej6/array_helpers.c
@@ -0,0 +1,52 @@
+#include "array_helpers.h"
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath) {
+    FILE *file = fopen(filepath, "r"); // Open mode READONLY
+
+    unsigned int length;
+    fscanf(file, "%u", &length);
+
+    if (max_size < length) {
+        printf("Only arrays with a maximum length of '%u' are allowed", max_size);
+        exit(EXIT_FAILURE);
+    }
+
+    int x;
+    for (unsigned int i = 0; i < length; i++) {
+        fscanf(file, "%d", &x);
+        array[i] = x;
+    }
+
+    fclose(file);
+
+    return length;
+}
+
+void array_dump(int a[], unsigned int length) {
+    printf("\n[");
+    for (unsigned int i = 0; i < length; i++) {
+        printf("%d", a[i]);
+
+        if (i + 1 < length) {
+            printf(", ");
+        }
+    }
+    printf("]\n\n");
+}
+
+bool array_is_sorted(int a[], unsigned int length) {
+    if (length == 0 || length == 1) { return true; }
+
+    for (unsigned int i = 0; i < length - 1; i++) {
+        if (a[i + 1] < a[i]) { return false; }
+    }
+
+    return true;
+}
+
+void array_swap(int a[], unsigned int i, unsigned int j) {
+    int aux = a[i];
+
+    a[i] = a[j];
+    a[j] = aux;
+}
