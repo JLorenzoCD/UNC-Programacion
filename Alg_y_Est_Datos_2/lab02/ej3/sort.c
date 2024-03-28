1cb0c1dff0d102d677e96a02ea0ce4fffb494dca
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-27 22:02:17 -0300

    Lab02 - Ej 3

--- Alg_y_Est_Datos_2/lab02/ej3/sort.c
+++ Alg_y_Est_Datos_2/lab02/ej3/sort.c
@@ -9,21 +9,39 @@
 
 
 static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
-
-    /* Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
+    /*
+    Permutes elements of a[izq..der] and returns pivot such that:
+        - izq <= pivot <= der
+        - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
+        - a[pivot] 'goes_before' all the elements in a(pivot,der]
     */
+    unsigned int ppiv = izq;
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+
+    while (i <= j) {
+        if (goes_before(a[i], a[ppiv])) { i++; }
+        else if (goes_before(a[ppiv], a[j])) { j--; }
+        else if (goes_before(a[ppiv], a[i]) && goes_before(a[j], a[ppiv])) {
+            swap(a, i, j);
+            i++;
+            j--;
+        }
+    }
+    swap(a, ppiv, j);
+    ppiv = j;
+
+    return ppiv;
 }
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
-
+    if (izq < der) {
+        unsigned int ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, (ppiv == 0u ? 0u : ppiv - 1));
+        quick_sort_rec(a, ppiv + 1, der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
-
--- Alg_y_Est_Datos_2/lab02/ej3/sort.h
+++ Alg_y_Est_Datos_2/lab02/ej3/sort.h
@@ -5,11 +5,10 @@
 
 void quick_sort(int a[], unsigned int length);
 /*
-    Sort the array 'a' using the Quicksort algorithm. The resulting sort
-    will be ascending according to the goes_before funtion.
+Sort the array 'a' using the Quicksort algorithm. The resulting sort
+will be ascending according to the goes_before funtion.
 
-    The array 'a' must have exactly 'length' elements.
-
+The array 'a' must have exactly 'length' elements.
 */
 
 #endif
