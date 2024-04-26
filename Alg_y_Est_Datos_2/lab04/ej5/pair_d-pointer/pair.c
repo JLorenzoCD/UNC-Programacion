4e5449c5d4c2c8b8b2c254e371d30038f56fd377
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-04-26 18:03:50 -0300

    Lab04 - Ej 5d

---
+++ Alg_y_Est_Datos_2/lab04/ej5/pair_d-pointer/pair.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pair.h"
+
+struct s_pair_t {
+    int fst;
+    int snd;
+};
+
+pair_t pair_new(int x, int y) {
+    pair_t p = NULL;
+
+    p = (pair_t)malloc(sizeof(struct s_pair_t));
+    if (p == NULL) {
+        fprintf(stderr, "Not enough memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    p->fst = x;
+    p->snd = y;
+
+    return p;
+}
+
+int pair_first(pair_t p) {
+    return p->fst;
+}
+
+int pair_second(pair_t p) {
+    return p->snd;
+}
+
+pair_t pair_swapped(pair_t p) {
+    return pair_new(p->snd, p->fst);
+}
+
+pair_t pair_destroy(pair_t p) {
+    free(p);
+    p = NULL;
+
+    return p;
+}
