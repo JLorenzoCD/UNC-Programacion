99cc03a691b4add8d9e0c2eb9540c4db2f201ace
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-04-26 18:07:43 -0300

    Lab04 - Ej 5e

---
+++ Alg_y_Est_Datos_2/lab04/ej5/pair_e-morf/pair.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pair.h"
+
+struct s_pair_t {
+    elem fst;
+    elem snd;
+};
+
+pair_t pair_new(elem x, elem y) {
+    pair_t p = NULL;
+
+    p = (pair_t)malloc(sizeof(struct s_pair_t));
+    if (p == NULL) {
+        fprintf(stderr, "Not enough memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    p->fst = x;
+    p->snd = y;
+
+    return p;
+}
+
+elem pair_first(pair_t p) {
+    return p->fst;
+}
+
+elem pair_second(pair_t p) {
+    return p->snd;
+}
+
+pair_t pair_swapped(pair_t p) {
+    return pair_new(p->snd, p->fst);
+}
+
+pair_t pair_destroy(pair_t p) {
+    free(p);
+    p = NULL;
+
+    return p;
+}
