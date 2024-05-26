317249daec955b8c40b7f96ade26ae66c387198a
Jose Lorenzo Canovas <lorenzo.canovas@mi.unc.edu.ar>
2024-05-26 22:59:56 +0000

    ODC - lab02 - Idea de circulo en C

--- Org_Compu/lab02/funs/circulo.s
+++ Org_Compu/lab02/funs/circulo.s
@@ -36,6 +36,54 @@ circulo:
     Si (x - x0)² + (y - y0)² <= r², entonces el pixel esta dentro del circulo
 
     Por lo tanto, debo recorrero con dos bucles todos los pixeles del cuadrado y checkear que este dentro de la formula, si lo esta pinto.
+
+
+    void circle(int x, int y, int r, int color) {
+        // Parte de arriba
+        int currXi = x - r;
+        int currYi = y;
+        int currR = r;
+
+        while (y - r <= currYi) {
+            currXi = calcular_nuevo_xi(x, y, r, currXi, currYi);
+            currR = x - currXi;
+            linea_recta_horizontal(currXi, currYi, x + currR, color);
+
+            currYi--;
+        }
+
+        // Parte de abajo
+        currXi = x - r;
+        currYi = y + 1u;
+        currR = r;
+
+        while (currYi <= y + r) {
+            currXi = calcular_nuevo_xi(x, y, r, currXi, currYi);
+            currR = x - currXi;
+            linea_recta_horizontal(currXi, currYi, x + currR, color);
+
+            currYi++;
+        }
+    }
+
+
+    int calcular_nuevo_xi(int x, int y, int r, int xi, int yi) {
+        int new_curr_xi = xi;
+
+        while (!cumple_ecuacion(x, y, r, new_curr_xi, yi)) {
+            new_curr_xi--;
+        }
+
+        return new_curr_xi;
+    }
+
+
+    bool cumple_ecuacion(int x, int  y, int r, int xi, int yi) {
+        int temp1 = (x - xi) * (x - xi);
+        int temp2 = (y - yi) * (y - yi);
+
+        return (temp1 + temp2) <= r * r;
+    }
     */
 
 
