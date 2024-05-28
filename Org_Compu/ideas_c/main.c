e4353e7cec9f843521d891a5144b3cbb6f61b17b
Jose Lorenzo Canovas <lorenzo.canovas@mi.unc.edu.ar>
2024-05-28 00:02:15 +0000

    ODC - lab02 - Corrigiendo archivos .c con las ideas para la ui

--- Org_Compu/ideas_c/main.c
+++ Org_Compu/ideas_c/main.c
@@ -41,24 +41,26 @@ void pintar_pixel(uint x, uint y, char color) {
 }
 
 // ······················· Linea recta ·······················
-void pintar_linea_recta_hor(uint x, uint y, uint xn, char color) {
-    for (uint xi = xn; x <= xi; xi--) {
+void pintar_linea_recta_hor(uint x, uint y, uint largo, char color) {
+    uint xn = x + largo;
+
+    for (uint xi = x; xi < xn; xi++) {
         pintar_pixel(xi, y, color);
     }
 }
 
 // ······················· Rectángulo ·······················
-void pintar_rectangulo(uint x, uint y, uint xn, uint yn, char color) {
-    uint yi = y;
-    while (yi <= yn) {
-        pintar_linea_recta_hor(x, yi, xn, color);
-        yi++;
+void pintar_rectangulo(uint x, uint y, uint ancho, uint alto, char color) {
+    uint yn = y + alto;
+
+    for (uint yi = y; yi < yn; yi++) {
+        pintar_linea_recta_hor(x, yi, ancho, color);
     }
 }
 
 // ······················· Cuadrado ·······················
 void pintar_cuadrado(uint x, uint y, uint largo, char color) {
-    pintar_rectangulo(x, y, x + largo, y + largo, color);
+    pintar_rectangulo(x, y, largo, largo, color);
 }
 
 // ······················· Circulo ·······················
@@ -80,88 +82,30 @@ void calcular_nuevo_xi(uint x, uint y, uint *xi, uint yi, uint r) {
 }
 
 void pintar_circulo(uint x, uint y, uint r, char color) {
-    // Parte de arriba
     uint xi = x + r;
-    uint yi = y;
     uint ri = r;
 
-    uint limite_superior = y - r;
-
-    while (limite_superior <= yi) {
-        calcular_nuevo_xi(x, y, &xi, yi, r);
+    for (uint k = 0u; k < r; k++) {
+        calcular_nuevo_xi(x, y, &xi, y + k, r);
         ri = xi - x;
-        pintar_linea_recta_hor(x - ri, yi, xi, color);
-
-        yi--;
+        pintar_linea_recta_hor(x - ri, y + k, ri * 2, color); // Zona superior
+        pintar_linea_recta_hor(x - ri, y - k, ri * 2, color); // Zona inferior
     }
 
-    // Parte de abajo
-    xi = x + r;
-    yi = y + 1u;
-    ri = r;
-
-    uint limite_inferior = y + r;
-
-    while (yi <= limite_inferior) {
-        calcular_nuevo_xi(x, y, &xi, yi, r);
-        ri = xi - x;
-        pintar_linea_recta_hor(x - ri, yi, xi, color);
-
-        yi++;
-    }
 }
 
 int main(void) {
 
     screen_init();
 
-    pintar_linea_recta_hor(3, 2, 80, '-');
+    // pintar_linea_recta_hor(3, 2, 80, '-');
 
-    pintar_rectangulo(5, 6, 15, 8, '#');
-    pintar_cuadrado(20, 20, 10, '+');
+    // pintar_rectangulo(5, 6, 15, 8, '#');
+    // pintar_cuadrado(20, 20, 10, '+');
 
-    pintar_circulo(50, 30, 7, '@');
+    pintar_circulo(50, 30, 4, '@');
 
     screen_dump();
 
     return EXIT_SUCCESS;
 }
-
-
-/*
-  ANSI_BRGOLD
-        "         _______..___________.     ___      .______             \n"
-        "        /       ||           |    /   \\     |   _  \\          \n"
-        "       |   (----``---|  |----`   /  ^  \\    |  |_)  |          \n"
-        "        \\   \\        |  |       /  /_\\  \\   |      /        \n"
-        "    .----)   |       |  |      /  _____  \\  |  |\\  \\----.    \n"
-        "    |_______/        |__|     /__/     \\__\\ | _| `._____|     \n"
-        "                                                                \n"
-        "____    __    ____      ___      .______           _______.     \n"
-        "\\   \\  /  \\  /   /     /   \\     |   _  \\         /       |\n"
-        " \\   \\/    \\/   /     /  ^  \\    |  |_)  |       |   (----` \n"
-        "  \\            /     /  /_\\  \\   |      /         \\   \\    \n"
-        "   \\    /\\    /     /  _____  \\  |  |\\  \\----..----)   |   \n"
-        "    \\__/  \\__/     /__/     \\__\\ | _| `._____||_______/     \n"
-        "\n\n\n"
-        "                           Episode II \n\n"
-        "                      ATTACK OF THE CLONES\n\n"
-        "                There is  unrest in the Galactic\n"
-        "                Senate. Several  thousand  solar\n"
-        "                systems  have   declared   their\n"
-        "                intentions to leave the Republic.\n\n"
-        "                This      separatist     movement,\n"
-        "                under  the   leadership   of  the\n"
-        "                mysterious   Count   Dooku,   has\n"
-        "                made it difficult for the limited\n"
-        "                number   of   Jedi   Knights   to\n"
-        "                maintain   peace   and  order  in\n"
-        "                the galaxy.\n\n"
-        "                Senator   Amidala,   the   former\n"
-        "                Queen   of  Naboo,  is  returning\n"
-        "                to  the  Galactic  Senate to vote\n"
-        "                on the critical issue of creating\n"
-        "                an    ARMY    OF   THE   REPUBLIC\n"
-        "                to    assist    the   overwhelmed\n"
-        "                Jedi....\n" ANSI_WHITE;
-*/
