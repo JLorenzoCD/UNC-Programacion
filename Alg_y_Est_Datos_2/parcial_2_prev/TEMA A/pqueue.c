8a8b2de47df390adbbe3c7e801e0f1447ac5a22c
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-05-29 20:04:16 -0300

    Alg 2 - Modelo parcial 2 lab - Resolución TEMA A

--- Alg_y_Est_Datos_2/parcial_2_prev/TEMA A/pqueue.c
+++ Alg_y_Est_Datos_2/parcial_2_prev/TEMA A/pqueue.c
@@ -4,124 +4,151 @@
 
 #include "pqueue.h"
 
+typedef struct s_node* list;
+
 struct s_pqueue {
-    /*
-     * COMPLETAR
-     *
-     */
+    list front;
+    size_t size;
 };
 
 struct s_node {
-    /*
-     * COMPLETAR
-     *
-     */
+    pqueue_elem elem;
+    unsigned int priority;
+    list next;
 };
 
-static struct s_node * create_node(pqueue_elem e, unsigned int priority) {
-    struct s_node *new_node = NULL;
-    // Completar
+static list create_node(pqueue_elem e, unsigned int priority) {
+    list new_node = NULL;
+
+    new_node = (list)malloc(sizeof(struct s_node));
     assert(new_node != NULL);
-    /*
-     * COMPLETAR
-     */
-    assert(e == e && priority == priority); // BORRAR ESTE ASSERT
+
+    new_node->priority = priority;
+    new_node->elem = e;
+    new_node->next = NULL;
+
     return new_node;
 }
 
-static struct s_node * destroy_node(struct s_node *node) {
+static list destroy_node(list node) {
     assert(node != NULL);
-    /*
-     * COMPLETAR
-     *
-     */
+
+    free(node);
+    node = NULL;
+
     assert(node == NULL);
     return node;
 }
 
 
 static bool invrep(pqueue q) {
-    /*
-     * COMPLETAR
-     *
-     */
-    assert(q == q); // BORRAR ESTE ASSERT
-    return true;
+    bool inv = q != NULL;
+
+    if (inv && q->size != 0u) {
+        inv = q->front != NULL;
+
+        unsigned int len = 0u;
+        list temp = q->front;
+        while (temp != NULL && inv) {
+            temp = temp->next;
+            len++;
+        }
+
+        inv = inv && len == q->size;
+    }
+
+    return inv;
 }
 
 pqueue pqueue_empty(void) {
     pqueue q = NULL;
-    /*
-     * COMPLETAR
-     *
-     */
+
+    q = (pqueue)malloc(sizeof(struct s_pqueue));
+    assert(q != NULL);
+
+    q->size = 0u;
+    q->front = NULL;
+
     return q;
 }
 
 pqueue pqueue_enqueue(pqueue q, pqueue_elem e, unsigned int priority) {
     assert(invrep(q));
-    struct s_node *new_node = create_node(e, priority);
-    /*
-     * COMPLETAR
-     *
-     */
-    assert(e == e && priority == priority && new_node == new_node); // BORRAR ESTE ASSERT
+    list new_node = create_node(e, priority);
+
+    if (q->front == NULL) {
+        q->front = new_node;
+    }
+    else {
+        list temp = q->front;
+
+        while (temp->priority <= new_node->priority && temp->next != NULL) {
+            temp = temp->next;
+        }
+
+        if (temp->next != NULL) {
+            new_node->next = temp->next;
+        }
+
+        temp->next = new_node;
+    }
+
+    q->size++;
+
     return q;
 }
 
 bool pqueue_is_empty(pqueue q) {
-    /*
-     * COMPLETAR
-     *
-     */
-    assert(q == q); // BORRAR ESTE ASSERT
-    return true;
+    return q->size == 0u;
 }
 
 pqueue_elem pqueue_peek(pqueue q) {
-    /*
-     * COMPLETAR
-     *
-     */
-    assert(q == q); // BORRAR ESTE ASSERT
-    return 0;
+    assert(invrep(q) && !pqueue_is_empty(q));
+
+    return q->front->elem;
 }
 
 unsigned int pqueue_peek_priority(pqueue q) {
-    /*
-     * COMPLETAR
-     *
-     */
-    assert(q == q); // BORRAR ESTE ASSERT
-    return 0;
+    assert(invrep(q) && !pqueue_is_empty(q));
+
+    return q->front->priority;
 }
 
 unsigned int pqueue_size(pqueue q) {
     assert(invrep(q));
     unsigned int size = 0;
-    /*
-     * COMPLETAR
-     *
-     */
+
+    size = q->size;
 
     return size;
 }
 
 pqueue pqueue_dequeue(pqueue q) {
-    /*
-     * COMPLETAR
-     *
-     */
-    assert(q == q); // BORRAR ESTE ASSERT
+    assert(invrep(q) && !pqueue_is_empty(q));
+
+    list temp = q->front;
+    q->front = temp->next;
+
+    temp->next = NULL;
+    temp = destroy_node(temp);
+
+    q->size--;
+
+    assert(invrep(q));
+
     return q;
 }
 
 pqueue pqueue_destroy(pqueue q) {
     assert(invrep(q));
-    /*
-     * COMPLETAR
-     *
-     */
+
+    while (!pqueue_is_empty(q)) {
+        q = pqueue_dequeue(q);
+    }
+
+    free(q);
+    q = NULL;
+
     assert(q == NULL);
     return q;
 }
