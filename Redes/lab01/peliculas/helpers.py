0cef404fc2b79f2b618ae3d5de912059f7b1024c
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-03-23 15:43:28 -0300

    Redes - Lab01 - Archivo 'helpers.py', se crea fun 'es_genero_valido'

--- Redes/lab01/peliculas/helpers.py
+++ Redes/lab01/peliculas/helpers.py
@@ -9,9 +9,12 @@ def validar_req_pelicula() -> tuple[str, str]:
     if(not titulo or not genero):
         raise Exception('Error: Los campos \'titulo\' y \'genero\'')
 
-    if(not genero in generos):
+    if(not es_genero_valido(genero)):
         raise Exception(
                 f"""Error: El genero '{genero}' no es un genero valido. Revisar
                 los géneros validos en la documentación."""
             )
     return (titulo, genero)
+
+def es_genero_valido(genero: str) -> bool:
+    return genero.lower() in generos
