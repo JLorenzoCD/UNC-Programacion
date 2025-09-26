23794cf98a8a4dcf316fa2eb650de8f839375138
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-09-26 14:29:30 -0300

    BD - TP4 - Ej6, se añade otra posible solución

--- Bases_Datos/TP4/p1_ej6.sql
+++ Bases_Datos/TP4/p1_ej6.sql
@@ -10,3 +10,12 @@ SELECT country.Name as CountryName, (
         WHERE city.CountryCode = country.Code
     ) as PopulationForCityMorePopulate
 FROM country;
+
+SELECT country.Name as CountryName, (
+        SELECT city.Population
+        FROM city
+        WHERE city.CountryCode = country.Code
+        ORDER BY city.Population DESC
+        LIMIT 1
+    ) as PopulationForCityMorePopulate
+FROM country;
