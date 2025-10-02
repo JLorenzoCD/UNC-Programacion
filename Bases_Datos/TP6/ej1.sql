6cc2d4e10bf8c5aeb6f8b88111dab10daac2378b
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-01 23:29:06 -0300

    BD - TP6 - Ej1

--- Bases_Datos/TP6/ej1.sql
+++ Bases_Datos/TP6/ej1.sql
@@ -2,4 +2,21 @@
 1.​ Devuelva la oficina con mayor número de empleados.
 */
 
+-- Si solo se pide el id
+SELECT e.officeCode
+FROM employees e
+GROUP BY e.officeCode
+ORDER BY COUNT(e.officeCode) DESC
+LIMIT 1;
 
+-- Si se pide toda la info de esta
+SELECT o.*
+FROM offices o
+INNER JOIN (
+	SELECT e.officeCode
+	FROM employees e
+	GROUP BY e.officeCode
+	ORDER BY COUNT(e.officeCode) DESC
+	LIMIT 1
+) as office_more_employees
+ON o.officeCode = office_more_employees.officeCode;
