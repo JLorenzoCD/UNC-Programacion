97eaa82127bcce3e284e77b22112d88ba82e5277
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-02 21:05:41 -0300

    BD - TP6 - Ej 3 y 4

--- Bases_Datos/TP6/ej3.sql
+++ Bases_Datos/TP6/ej3.sql
@@ -2,4 +2,12 @@
 3.​ Devolver el valor promedio, máximo y mínimo de pagos que se hacen por mes.
 */
 
-
+SELECT
+	YEAR(p.paymentDate) AS year,
+	MONTH(p.paymentDate) AS month,
+	AVG(p.amount) AS prom,
+	MAX(p.amount) AS max,
+	MIN(p.amount) AS min
+FROM payments p
+GROUP BY year, month
+ORDER BY year, month;
--- Bases_Datos/TP6/ej4.sql
+++ Bases_Datos/TP6/ej4.sql
@@ -3,4 +3,18 @@
 crédito de un cliente con un valor pasado por parámetro.
 */
 
+DELIMITER $$
+CREATE PROCEDURE UpdateCredit(IN customerNumber INT, IN newCreditLimit DECIMAL(10,2))
+BEGIN
+    IF EXISTS (SELECT 1 FROM customers c WHERE c.customerNumber = customerNumber) THEN
+        UPDATE customers c
+        SET c.creditLimit = newCreditLimit
+        WHERE c.customerNumber = customerNumber;
+    END IF;
+END$$
+DELIMITER ;
 
+CALL UpdateCredit(103, 20.00)
+
+-- Reset
+CALL UpdateCredit(103, 21000.00);
