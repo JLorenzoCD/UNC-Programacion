32926d27d56c76043f547666a7b6d97c5f3b7023
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-17 16:07:00 -0300

    BD - TP7 - initial files

---
+++ Bases_Datos/TP7/ej1.mongodb.js
@@ -0,0 +1,6 @@
+/*
+1.​ Insertar 5 nuevos usuarios en la colección users. Para cada nuevo usuario creado,
+insertar al menos un comentario realizado por el usuario en la colección comments.
+*/
+
+use("mflix")
