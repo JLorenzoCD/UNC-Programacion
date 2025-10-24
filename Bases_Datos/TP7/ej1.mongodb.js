0320324c31b4b165ddf108bb53a90b567b8aa7f2
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-24 12:08:29 -0300

    TP7 - Ej 1

--- Bases_Datos/TP7/ej1.mongodb.js
+++ Bases_Datos/TP7/ej1.mongodb.js
@@ -4,3 +4,55 @@ insertar al menos un comentario realizado por el usuario en la colección commen
 */
 
 use("mflix")
+
+/* db.users.insertMany(
+    [
+        {
+            name: "Víctor Gago",
+            email: 'víctor.gago@email.com',
+            password: "123456789",
+        },
+        {
+            name: "Guillem Hermida",
+            email: 'guillem.hermida@email.com',
+            password: "123456789",
+        },
+        {
+            name: "María Fernanda Marques",
+            email: 'maría.fernanda.marques@email.com',
+            password: "123456789",
+        },
+        {
+            name: "Sacramento Pino",
+            email: 'sacramento.pino@email.com',
+            password: "123456789",
+        },
+        {
+            name: "Aina García",
+            email: 'aina.garcía@email.com',
+            password: "123456789",
+        },
+    ]
+) */
+
+const users = db.users.find(
+    { name: { $in: ["Víctor Gago", "Guillem Hermida", "María Fernanda Marques", "Sacramento Pino", "Aina García"] } }
+).toArray()
+
+const movie = db.movies.findOne()
+
+const commentsToInsert = []
+for (const user of users) {
+    const newComment = {
+        name: user.name,
+        email: user.email,
+        movie_id: movie._id,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        date: new Date(),
+    }
+
+    commentsToInsert.push(newComment)
+}
+
+// db.comments.insertMany(commentsToInsert)
+
