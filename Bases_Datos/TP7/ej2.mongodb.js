bebd431cb227c80fb6a2586a308ab430e0586e00
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-25 18:41:07 -0300

    BD - TP7 - Ej 2

--- Bases_Datos/TP7/ej2.mongodb.js
+++ Bases_Datos/TP7/ej2.mongodb.js
@@ -6,3 +6,23 @@ valor de “imdb.rating” sea de tipo “double”).
 */
 
 use("mflix")
+
+db.movies.find(
+    {
+        year: { $gte: 1990, $lt: 2000 },
+        "imdb.rating": { $type: "double" }
+    },
+    {
+        _id: 0,
+        title: 1,
+        year: 1,
+        cast: 1,
+        directors: 1,
+        rating: "$imdb.rating"
+    },
+).sort(
+    {
+        "imdb.rating": -1,
+        title: 1,
+    }
+).limit(10)
