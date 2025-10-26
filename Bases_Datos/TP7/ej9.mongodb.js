4e065790ae68a84f55443d063de954a844201ad3
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-26 12:25:43 -0300

    BD - TP 7 - Ej 9

--- Bases_Datos/TP7/ej9.mongodb.js
+++ Bases_Datos/TP7/ej9.mongodb.js
@@ -4,3 +4,16 @@ victor_patel@fakegmail.com durante el año 1980.
 */
 
 use("mflix")
+
+const year1980 = new Date("1980-01-01T00:00:00.000Z");
+const year1981 = new Date("1981-01-01T00:00:00.000Z");
+
+db.comments.deleteMany(
+    {
+        email: "victor_patel@fakegmail.com",
+        date: {
+            $gte: year1980,
+            $lt: year1981,
+        }
+    }
+)
