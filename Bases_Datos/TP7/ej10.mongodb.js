960a44d6b28b304862d912f3ad449c57b95dee59
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-26 12:40:32 -0300

    DB - TP7 - Parte 2 - init files

---
+++ Bases_Datos/TP7/ej10.mongodb.js
@@ -0,0 +1,10 @@
+/*
+10.​Listar el id del restaurante (restaurant_id) y las calificaciones de los
+restaurantes donde al menos una de sus calificaciones haya sido realizada entre
+2014 y 2015 inclusive, y que tenga una puntuación (score) mayor a 70 y menor o
+igual a 90.
+*/
+
+use("restaurantdb")
+
+db.restaurants.findOne()
---
+++ Bases_Datos/TP7/ej11.mongodb.js
@@ -0,0 +1,21 @@
+/*
+11.​Agregar dos nuevas calificaciones al restaurante cuyo id es "50018608". A
+continuación se especifican las calificaciones a agregar en una sola consulta.
+
+{
+    "date" : |SODate("2019-10-10T00:00:00Z"),
+    "grade" : "A",
+    "score" : 18
+}
+
+{
+    "date" : |SODate("2020-02-25T00:00:00Z"),
+    "grade" : "A",
+    "score" : 21
+}
+
+*/
+
+use("restaurantdb")
+
+db.restaurants.findOne()
