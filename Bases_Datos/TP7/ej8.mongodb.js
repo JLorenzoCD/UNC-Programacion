48a9338ed82431553784811bd71cdd2d4caeed42
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-26 12:21:24 -0300

    BD - TP7 - Ej 8

--- Bases_Datos/TP7/ej8.mongodb.js
+++ Bases_Datos/TP7/ej8.mongodb.js
@@ -6,3 +6,23 @@ dos veces. ¿Qué operación se realiza en cada caso? (Hint: usar upserts).
 */
 
 use("mflix")
+
+db.users.updateOne(
+    {
+        email: "joel.macdonel@fakegmail.com"
+    },
+    {
+        $set: {
+            password: "some password"
+        }
+    },
+    {
+        upsert: true
+    }
+)
+
+/*
+En la primera ejecución, se inserta un nuevo usuario ya que el email no se encuentra.
+En la segunda ejecución, se actualiza el usuario recién creado, aunque los valores
+son los mismos, por lo que en realidad no hace nada.
+*/
