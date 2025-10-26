e379998a78633d2f3c0165ce4aa323e863ae8131
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-26 12:11:29 -0300

    DB - TP7 - Ej 7

--- Bases_Datos/TP7/ej7.mongodb.js
+++ Bases_Datos/TP7/ej7.mongodb.js
@@ -5,3 +5,15 @@ comentario" y fecha actual respectivamente.
 */
 
 use("mflix")
+
+db.comments.updateOne(
+    {
+        _id: ObjectId("5b72236520a3277c015b3b73")
+    },
+    {
+        $set: {
+            text: "mi mejor comentario",
+            date: new Date(),
+        }
+    }
+)
