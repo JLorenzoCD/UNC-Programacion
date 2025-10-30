bf83d6fdc240e9b3b0928e0b076d795bdfb2e2fb
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-30 20:09:34 -0300

    BD - TP8 - Ej7

--- Bases_Datos/TP8/ej7.mongodb.js
+++ Bases_Datos/TP8/ej7.mongodb.js
@@ -4,4 +4,43 @@ en los años 80 (desde 1980 hasta 1989), ordenados de mayor a menor por promedio
 del año.
 */
 
+use("mflix")
+
+db.movies.aggregate([
+    {
+        $match: {
+            year: {
+                $gte: 1980,
+                $lte: 1989,
+            },
+            "imdb.rating": {
+                $type: "number"
+            }
+        }
+    },
+    {
+        $group: {
+            _id: "$year",
+            avg_rating: { $avg: "$imdb.rating" },
+            min_rating: { $min: "$imdb.rating" },
+            max_rating: { $max: "$imdb.rating" },
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            year: "$_id",
+            rating: {
+                avg: "$avg_rating",
+                min: "$min_rating",
+                max: "$max_rating",
+            }
+        }
+    },
+    {
+        $sort: {
+            "rating.avg": -1
+        }
+    },
+])
 
