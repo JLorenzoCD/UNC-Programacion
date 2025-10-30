f1598fec589ca35d1f22e0d01cd8e2d29a61e26c
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-30 15:01:53 -0300

    BD - TP8 - Ej2

--- Bases_Datos/TP8/ej2.mongodb.js
+++ Bases_Datos/TP8/ej2.mongodb.js
@@ -2,4 +2,34 @@
 2.​ Cantidad de estados con al menos dos cines (theaters) registrados.
 */
 
+use("mflix")
 
+db.theaters.aggregate([
+    {
+        $group: {
+            _id: "$location.address.state",
+            total_theaters: {
+                $sum: 1
+            }
+        }
+    },
+    {
+        $match: {
+            total_theaters: {
+                $gte: 2
+            },
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            number_states_with_at_least_2_theaters: { $sum: 1 }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            number_states_with_at_least_2_theaters: 1
+        }
+    }
+])
