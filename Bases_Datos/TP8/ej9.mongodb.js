0abbfd2ed0697db68f76bbf7b23fc87a3b52b489
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-30 20:44:35 -0300

    BD - TP8 - Ej9

--- Bases_Datos/TP8/ej9.mongodb.js
+++ Bases_Datos/TP8/ej9.mongodb.js
@@ -3,4 +3,63 @@
 con la cantidad de comentarios.
 */
 
+use("mflix")
 
+db.createView(
+    "genresTop5MostCommented",
+    "comments",
+    [
+        {
+            $group: {
+                _id: "$movie_id",
+                comments_count: {
+                    $sum: 1
+                }
+            }
+        },
+        {
+            $lookup: {
+                from: "movies",
+                localField: "_id",
+                foreignField: "_id",
+                as: "movie"
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                comments_count: 1,
+
+                genres: { $arrayElemAt: ["$movie.genres", 0] }
+            }
+        },
+        {
+            $unwind: "$genres",
+        },
+        {
+            $group: {
+                _id: "$genres",
+                comments_count_for_genre: {
+                    $sum: "$comments_count"
+                }
+            }
+        },
+        {
+            $sort: {
+                comments_count_for_genre: -1
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                genre: "$_id",
+                comments_count_for_genre: 1,
+            }
+        },
+        {
+            $limit: 5
+        }
+    ]
+)
+
+db.genresTop5MostCommented.find()
