d0352f41030a120626c6474a0c38b6273cf9429f
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-30 19:54:08 -0300

    BD - TP8 - Ej6

--- Bases_Datos/TP8/ej6.mongodb.js
+++ Bases_Datos/TP8/ej6.mongodb.js
@@ -3,4 +3,34 @@
 y Cantidad de Comentarios.
 */
 
+use("mflix")
 
+db.comments.aggregate([
+    {
+        $group: {
+            _id: {
+                name: "$name",
+                email: "$email"
+            },
+            comments_count: {
+                $sum: 1
+            },
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            name: "$_id.name",
+            email: "$_id.email",
+            comments_count: 1
+        }
+    },
+    {
+        $sort: {
+            comments_count: -1
+        }
+    },
+    {
+        $limit: 10
+    }
+])
