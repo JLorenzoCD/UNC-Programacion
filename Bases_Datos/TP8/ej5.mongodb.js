c57a266af91205b183209f78a1c10dd722ce3802
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-30 19:47:35 -0300

    BD - TP8 - Ej5

--- Bases_Datos/TP8/ej5.mongodb.js
+++ Bases_Datos/TP8/ej5.mongodb.js
@@ -4,4 +4,33 @@ las películas pueden tener más de un género). Devolver el género y la cantid
 de películas. Hint: unwind puede ser de utilidad
 */
 
+use("mflix")
 
+db.movies.aggregate([
+    {
+        $unwind: "$genres"
+    },
+    {
+        $group: {
+            _id: "$genres",
+            genre_count: {
+                $sum: 1
+            },
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            genre: "$_id",
+            genre_count: 1
+        }
+    },
+    {
+        $sort: {
+            genre_count: -1
+        }
+    },
+    {
+        $limit: 10
+    }
+])
