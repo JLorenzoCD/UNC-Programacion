d2bb7ef2ec4df3eb3730334d4d4cd64d4ee6abc8
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-31 09:42:08 -0300

    BD - TP8 - Ej12

--- Bases_Datos/TP8/ej12.mongodb.js
+++ Bases_Datos/TP8/ej12.mongodb.js
@@ -9,4 +9,108 @@ mínima y la suma total. Se puede asumir que el restaurant_id es único.
     d.​ Resolver con find.
 */
 
+use("restaurantdb")
 
+// a
+db.restaurants.aggregate([
+    {
+        $unwind: "$grades"
+    },
+    {
+        $group: {
+            _id: "$restaurant_id",
+            name: { $first: "$name" },
+            sum_grade: {
+                $sum: "$grades.score"
+            },
+            max_grade: {
+                $max: "$grades.score"
+            },
+            min_grade: {
+                $min: "$grades.score"
+            },
+        }
+    }
+])
+
+// b
+db.restaurants.aggregate([
+    {
+        $match: {
+            "grades.0": {
+                $exists: true,
+            }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            name: 1,
+            restaurant_id: 1,
+            sum_grade: {
+                $sum: "$grades.score"
+            },
+            max_grade: {
+                $max: "$grades.score"
+            },
+            min_grade: {
+                $min: "$grades.score"
+            },
+        }
+    }
+])
+
+// c
+db.restaurants.aggregate([
+    {
+        $match: {
+            "grades.0": {
+                $exists: true,
+            }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            name: 1,
+            restaurant_id: 1,
+
+            sum_grade: {
+                $reduce: {
+                    input: "$grades",
+                    initialValue: 0,
+                    in: { $add: ["$$value", "$$this.score"] },
+                }
+            },
+            max_grade: {
+                $max: "$grades.score"
+            },
+            min_grade: {
+                $min: "$grades.score"
+            },
+        }
+    }
+])
+
+// d
+db.restaurants.find(
+    {
+        "grades.0": {
+            $exists: true,
+        }
+    },
+    {
+        _id: 0,
+        name: 1,
+        restaurant_id: 1,
+        sum_grade: {
+            $sum: "$grades.score"
+        },
+        max_grade: {
+            $max: "$grades.score"
+        },
+        min_grade: {
+            $min: "$grades.score"
+        },
+    }
+)
