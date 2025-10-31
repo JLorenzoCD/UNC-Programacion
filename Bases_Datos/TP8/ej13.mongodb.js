7914347a0d794e0b4a8f297f8300b336e19460e9
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-31 09:57:08 -0300

    BD - TP8 - Ej13

--- Bases_Datos/TP8/ej13.mongodb.js
+++ Bases_Datos/TP8/ej13.mongodb.js
@@ -10,4 +10,53 @@
     b.​ HINT2. El operador $switch o $cond pueden ser de ayuda.
 */
 
+use("restaurantdb")
 
+db.restaurants.updateMany(
+    {   // Se modifican todos los elementos que tienen puntuación
+        "grades.0": {
+            $exists: true,
+        }
+    },
+    [
+        {
+            $addFields: {
+                average_score: {
+                    $avg: "$grades.score"
+                }
+            }
+        },
+        {
+            $addFields: {
+                grade: {
+                    $switch: {
+                        branches: [
+                            {
+                                case: {
+                                    $lte: ["$average_score", 13]
+                                },
+                                then: "A",
+                            },
+                            {
+                                case: {
+                                    $and: [
+                                        { $gt: ["$average_score", 13] },
+                                        { $lte: ["$average_score", 27] }
+                                    ]
+                                },
+                                then: "B",
+                            },
+                            {
+                                case: {
+                                    $gt: ["$average_score", 27]
+                                },
+                                then: "C",
+                            },
+                        ],
+                        default: "" // Posiblemente pueda haber score negativo o algo raro
+                    }
+                },
+            }
+        }
+    ]
+)
