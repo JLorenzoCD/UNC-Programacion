053d104ff3d4b794d98e332f4f9cbf7745427e09
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-31 10:53:47 -0300

    BD - TP9 - Ej3

--- Bases_Datos/TP9/ej3.mongodb.js
+++ Bases_Datos/TP9/ej3.mongodb.js
@@ -12,8 +12,72 @@ object con:
     valores posibles “Point” o null y coordinates que debe ser una lista de 2
     doubles.
 
-Por último, estas reglas de validación no deben prohibir la inserción o actualización de
-documentos que no las cumplan sino que solamente deben advertir.
+Por último, estas reglas de validación no deben prohibir la inserción o
+actualización de documentos que no las cumplan sino que solamente deben advertir.
 */
 
 use("mflix")
+
+// db.runCommand({
+//     collMod: "theaters",
+//     validator: {
+//         $jsonSchema: {
+//             bsonType: "object",
+//             required: ["theaterId", "location"],
+//             properties: {
+//                 theaterId: {
+//                     bsonType: "int",
+//                 },
+//                 location: {
+//                     bsonType: "object",
+//                     required: ["address"],
+//                     properties: {
+//                         address: {
+//                             bsonType: "object",
+//                             required: ["street1", "city", "state", "zipcode"],
+//                             properties: {
+//                                 street1: {
+//                                     bsonType: "string",
+//                                 },
+//                                 city: {
+//                                     bsonType: "string",
+//                                 },
+//                                 state: {
+//                                     bsonType: "string",
+//                                 },
+//                                 zipcode: {
+//                                     bsonType: "string",
+//                                 },
+//                             }
+//                         },
+//                         geo: {
+//                             bsonType: "object",
+//                             properties: {
+//                                 type: {
+//                                     enum: ["Point", null]
+//                                 },
+//                                 coordinates: {
+//                                     bsonType: "array",
+//                                     items: { bsonType: "double"},
+//                                     maxItems: 2,
+//                                     minItems: 2,
+//                                 }
+//                             }
+//                         },
+//                     },
+//                 },
+//             }
+//         }
+//     },
+
+//     // Parámetros para solo advertir, no prohibir:
+//     validationAction: "warn", // Solo advierte (loggea), no previene la inserción
+//     validationLevel: "moderate" // Aplica reglas a inserciones y actualizaciones de docs existentes
+// })
+
+const { invalidTheaters, validTheaters } = require("./utils.js")
+
+db.theaters.insertMany([
+    ...invalidTheaters,
+    ...validTheaters,
+])
--- Bases_Datos/TP9/utils.js
+++ Bases_Datos/TP9/utils.js
@@ -51,4 +51,96 @@ const validUsers = [
     }
 ]
 
-module.exports = { invalidUsers, validUsers }
+const invalidTheaters = [
+    {},
+    {
+        theaterId: "10000",
+    },
+    {
+        theaterId: 10003,
+    },
+    {
+        theaterId: 10003,
+        location: null,
+    },
+    {
+        theaterId: 10003,
+        location: {
+            address: 50,
+        }
+    },
+]
+
+const validTheaters = [
+    {
+        theaterId: 10000,
+        location: {
+            address: {
+                street1: "340 W Market",
+                city: "Bloomington",
+                state: "MN",
+                zipcode: "55425"
+            },
+        }
+    },
+    {
+        theaterId: 10003,
+        location: {
+            address: {
+                street1: "45235 Worth Ave.",
+                city: "California",
+                state: "MD",
+                zipcode: "20619"
+            },
+        }
+    },
+    {
+        theaterId: 10008,
+        location: {
+            address: {
+                street1: "1621 E Monte Vista Ave",
+                city: "Vacaville",
+                state: "CA",
+                zipcode: "95688"
+            },
+            geo: {
+                type: "Point",
+                coordinates: [
+                    -121.96328,
+                    38.367649
+                ]
+            }
+        }
+    },
+    {
+        theaterId: 10004,
+        location: {
+            address: {
+                street1: "5072 Pinnacle Sq",
+                city: "Birmingham",
+                state: "AL",
+                zipcode: "35235"
+            },
+        }
+    },
+    {
+        theaterId: 10002,
+        location: {
+            address: {
+                street1: "1900 E Rio Salado Pkwy",
+                city: "Tempe",
+                state: "AZ",
+                zipcode: "85281"
+            },
+            geo: {
+                type: "Point",
+                coordinates: [
+                    -111.89966,
+                    33.430729
+                ]
+            }
+        }
+    }
+]
+
+module.exports = { invalidUsers, validUsers, invalidTheaters, validTheaters }
