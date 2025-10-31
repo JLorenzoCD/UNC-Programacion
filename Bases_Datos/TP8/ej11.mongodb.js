b17699dec395ca9fb3c6b15321dc33df3b31e932
lorenzo.canovas <jlorenzocanovas@gmail.com>
2025-10-30 23:37:56 -0300

    BD - TP8 - Ej11

--- Bases_Datos/TP8/ej11.mongodb.js
+++ Bases_Datos/TP8/ej11.mongodb.js
@@ -5,4 +5,106 @@ comentario, título de la película, fecha de lanzamiento. HINT: usar $lookup co
 multiple condiciones
 */
 
+use("mflix")
 
+db.comments.aggregate([
+    {
+        $lookup: {
+            from: "movies",
+            localField: "movie_id",
+            foreignField: "_id",
+            as: "movie"
+        }
+    },
+    {
+        $unwind: "$movie"
+    },
+    {
+        $match: {
+            "movie.released": { $exists: true, $ne: null },
+            $expr: {
+                $and: [
+                    { $eq: [{ $year: "$date" }, { $year: "$movie.released" }] },
+                    { $eq: [{ $month: "$date" }, { $month: "$movie.released" }] },
+                ]
+            }
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            nombre: "$name",
+            email: "$email",
+            fecha_comentario: "$date",
+            titulo_pelicula: "$movie.title",
+            fecha_lanzamiento: "$movie.released",
+        }
+    },
+])
+
+
+//? No se si esta bien esta otra version.
+// db.movies.aggregate([
+//     {
+//         $match: {
+//             released: {
+//                 $exists: true,
+//                 $ne: null,
+//             }
+//         }
+//     },
+//     {
+//         $lookup: {
+//             from: "comments",
+//             let: {
+//                 released: "$released",
+//                 movie: "$_id"
+//             },
+//             pipeline: [
+//                 {
+//                     $match: {
+//                         $expr: {
+//                             $and: [
+//                                 {
+//                                     $eq: ["$movie_id", "$$movie"],
+//                                 },
+//                                 {
+//                                     $eq: [
+//                                         { $year: "$date" },
+//                                         { $year: "$$released" },
+//                                     ]
+//                                 },
+//                                 {
+//                                     $eq: [
+//                                         { $month: "$date" },
+//                                         { $month: "$$released" },
+//                                     ]
+//                                 }
+//                             ]
+//                         }
+//                     }
+//                 },
+//             ],
+//             as: "comments",
+//         }
+//     },
+//     {
+//         $match: {
+//             "comments.0": {
+//                 $exists: true
+//             }
+//         }
+//     },
+//     {
+//         $unwind: "$comments"
+//     },
+//     {
+//         $project: {
+//             _id: 0,
+//             released: 1,
+//             name: "$comments.name",
+//             email: "$comments.email",
+//             date: "$comments.date",
+//         }
+//     },
+// ])
