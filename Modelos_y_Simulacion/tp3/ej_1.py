2b41ccaf5a44bb5019cb4f4d229304e01e51a100
lorenzo.canovas <jlorenzocanovas@gmail.com>
2026-04-08 22:31:02 -0300

    MyS - TP3 - Ej 1 - Gráficos

--- Modelos_y_Simulacion/tp3/ej_1.py
+++ Modelos_y_Simulacion/tp3/ej_1.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+
+# Generadores
+
+
 def generador_psda_x(x_seed=1):
     x = x_seed
 
@@ -32,3 +37,36 @@ def generador_psda_z(x_seed=1, y_seed=1):
 # for _ in range(50):
 #     z = next(generador)
 #     print(z)
+
+def obtener_pares(generador, n=100):
+    secuencia = [next(generador) for _ in range(n + 1)]
+
+    actuales = secuencia[:-1]  # Se elimina el ultimo
+    siguientes = secuencia[1:]  # Se elimina el primero
+    return actuales, siguientes
+
+
+# Config de los gráficos
+n_puntos = 500  # Cantidad de puntos a graficar
+fig, axes = plt.subplots(1, 3, figsize=(18, 5))
+fig.suptitle(
+    f'Gráficos de Retardo ($i$ vs $i+1$) para n={n_puntos}', fontsize=16)
+
+# Nombres y generadores para iterar
+nombres = ['Generador X (Mod 31)', 'Generador Y (Mod 32)',
+           'Generador Z (Combinado)']
+generadores = [generador_psda_x(), generador_psda_y(), generador_psda_z()]
+colores = ['blue', 'green', 'red']
+
+for i, ax in enumerate(axes):
+    x_coords, y_coords = obtener_pares(generadores[i], n_puntos)
+
+    ax.scatter(x_coords, y_coords,
+               color=colores[i], alpha=0.7, edgecolors='black')
+    ax.set_title(nombres[i])
+    ax.set_xlabel('$Valores_{i}$')
+    ax.set_ylabel('$Valores_{i+1}$')
+    ax.grid(True, linestyle='--', alpha=0.6)
+
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.show()
