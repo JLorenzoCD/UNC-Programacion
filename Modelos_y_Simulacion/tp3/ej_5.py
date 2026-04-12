69d3d6a5c7f989401ef7d9cd81f33a87886dd0ac
lorenzo.canovas <jlorenzocanovas@gmail.com>
2026-04-12 16:52:46 -0300

    MyS - TP3 - Ej 5b

--- Modelos_y_Simulacion/tp3/ej_5.py
+++ Modelos_y_Simulacion/tp3/ej_5.py
@@ -17,7 +17,26 @@ def punto_a(n: int):
     print(f"{est:.4f} \t | \t {v_real:.4f}")
 
 
+def punto_b(n: int):
+    def g(u: int):
+        return u/((u**2) - 1)
+
+    puntos = [random.random() for _ in range(n)]
+    integrales = [g(u + 2) for u in puntos]
+
+    est = np.mean(integrales)
+
+    v_real = 0.4904
+    print(f"Punto b - nro iteraciones ({n})")
+    print("Estimación \t|\t Valor real")
+    print(f"{est:.4f} \t | \t {v_real:.4f}")
+
+
 punto_a(1_000)
 punto_a(5_000)
 punto_a(10_000)
+print("\n")
+punto_b(1_000)
+punto_b(5_000)
+punto_b(10_000)
 print("\n")
