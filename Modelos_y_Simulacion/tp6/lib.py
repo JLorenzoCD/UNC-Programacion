56c676252c1d9128518153c3099a91d99278562d
lorenzo.canovas <jlorenzocanovas@gmail.com>
2026-05-28 10:18:01 -0300

    MyS - TP6 - lib - fix

--- Modelos_y_Simulacion/tp6/lib.py
+++ Modelos_y_Simulacion/tp6/lib.py
@@ -1,17 +1,17 @@
 
-def prom_update(prom_prev: float, x_new: float, n_prev: int) -> tuple[float, int]:
-    if n_prev < 1:
+def prom_update(prom_prev: float, x_new: float, n_curr: int):
+    if n_curr < 1:
         raise Exception()
-    elif n_prev == 1:
-        return x_new, n_prev + 1
+    elif n_curr == 1:
+        return x_new
     else:
-        return prom_prev + (x_new - prom_prev) / (n_prev + 1), n_prev + 1
+        return prom_prev + (x_new - prom_prev) / n_curr
 
 
-def s_cuad_update(s_cuad_prev: float, prom_prev: float, prom_curr: float, n_prev: int) -> tuple[float, int]:
-    if n_prev < 1:
+def s_cuad_update(s_cuad_prev: float, prom_prev: float, prom_curr: float, n_curr: int):
+    if n_curr < 1:
         raise Exception()
-    elif n_prev == 1:
-        return 0, n_prev + 1
+    elif n_curr == 1:
+        return 0.0
     else:
-        return (1 - 1/n_prev) * s_cuad_prev**2 + (n_prev + 1) * (prom_curr - prom_prev)**2
+        return (1 - (1/(n_curr - 1))) * s_cuad_prev + n_curr * ((prom_curr - prom_prev)**2)
