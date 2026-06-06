b1d890a5347bdc72aed4c4503c5004e9481e1bec
lorenzo.canovas <jlorenzocanovas@gmail.com>
2026-06-05 22:38:29 -0300

    MyS - TP7 - lib

---
+++ Modelos_y_Simulacion/tp7/lib.py
@@ -0,0 +1,70 @@
+from typing import Callable
+from scipy.stats import chi2
+import numpy as np
+
+# ------------------------------ Discretas ------------------------------
+
+
+def est_T(frecuencias: list[int], probabilidades: list[float], n: int, K: int):
+    t = 0
+    for k in range(K):
+        t = t + (frecuencias[k] - n * probabilidades[k]
+                 )**2 / (n * probabilidades[k])
+
+    return t
+
+
+def gen_frecuencias_binomial(probabilidades: list[float], n: int, K: int) -> list[int]:
+    # Genera directamente las frecuencias para las variables discretas ya que
+    # al no estimar ningún parámetro, no es necesario generar muestras
+
+    n_res = n
+    freq = []
+    p = probabilidades[0]
+    for i in range(K - 1):
+        N_i = np.random.binomial(n_res, p)
+        freq.append(N_i)
+
+        n_res -= N_i
+        p = probabilidades[i + 1] / (1 - sum(probabilidades[:(i + 1)]))
+
+    freq.append(n_res)
+
+    return freq
+
+
+def estimar_p_valor_pearson(frecuencias: list[int], probabilidades: list[float]):
+    K = len(frecuencias)
+    n = sum(frecuencias)
+
+    t_obs = est_T(frecuencias, probabilidades, n, K)
+    p_valor = chi2.sf(t_obs, df=(K - 1))
+
+    # print("p-valor: "", 1 - chi2.cdf(t_obs, df=(K - 1)))
+    print("p-valor: ", p_valor)
+
+
+def estimar_p_valor_simulaciones_discreta(
+    frecuencias: list[int],
+    probabilidades: list[float],
+    gen_freq: Callable[[list[float]], list[float]],
+    N_SIM: int = 10_000
+):
+
+    n = sum(frecuencias)
+    K = len(frecuencias)
+    t_obs = est_T(frecuencias, probabilidades, n, K)
+
+    p_valor = 0
+    for _ in range(N_SIM):
+        # generar lista de frecuencias utilizando la lista de probabilidades
+        freq = gen_freq(probabilidades, n, K)
+
+        # calculo el estadístico para la muestra generada
+        t = est_T(freq, probabilidades, n, K)
+
+        if t_obs <= t:
+            p_valor += 1
+
+    p_valor = p_valor / N_SIM
+    print("p-valor: ", p_valor)
