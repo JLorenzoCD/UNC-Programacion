262def7c1c960152178cffcb67a62bae806e7486
lorenzo.canovas <jlorenzocanovas@gmail.com>
2026-06-10 20:14:40 -0300

    MyS - TP5 - implementaciones de poisson_homogeneo, poisson_no_homogeneo_adelgazamiento y poisson_no_homogeneo_adelgazamiento_mejorado

--- Modelos_y_Simulacion/tp5/lib.py
+++ Modelos_y_Simulacion/tp5/lib.py
@@ -1,5 +1,6 @@
 import math
 import random
+from typing import Callable
 
 
 def exponencial(lamda: float):
@@ -26,6 +27,88 @@ def poisson_Exp(lamda: float):
         X += 1
 
     return X
+
+
+def poisson_homogeneo(lamda: float, T: float) -> tuple[int, list[float]]:
+    eventos = []
+    t = -math.log(1 - random.random()) / lamda
+
+    while t < T:
+        eventos.append(t)
+        t -= math.log(1 - random.random()) / lamda
+
+    return eventos
+
+
+def poisson_no_homogeneo_adelgazamiento(
+    T: float,
+    lamda_t: Callable[[float], float],
+    lamda_cota: float
+):
+    'lamda_t(t): intensidad, lamda_t(t) <= lamda_cota'
+
+    eventos = []
+    t = -math.log(1 - random.random()) / lamda_cota
+
+    while t <= T:
+        V = random.random()
+        if V < lamda_t(t) / lamda_cota:
+            eventos.append(t)
+
+        t -= math.log(1 - random.random()) / lamda_cota
+
+    return eventos
+
+
+def poisson_no_homogeneo_adelgazamiento_mejorado(
+    T: float,
+    lamda_t: Callable[[float], float],
+    interv: list[float],
+    lamda_cota: list[float]
+):
+    """
+    Se asume que t siempre comienza de 0, por lo que, a modo de ejemplo:
+        lamda_t = lambda t: 2*t + 1
+        interv = [2, 4, 6]
+
+        Donde interv representaría los siguientes intervalos:
+        I_0 = [0, 2]; I_1 = (2, 4]; I_2 = = (4, 6]
+
+        y podríamos tomar
+        lamda_cota = [5, 9, 13]
+
+    Tendríamos que:
+        assert len(interv) == len(lamda_cota)
+        assert lamda_t(t) <= lamda_cota[j], siendo j el intervalo en el cual se encuentra t
+        assert T <= interv[-1]
+        assert interv == sorted(interv)
+    """
+
+    if T > max(interv):
+        raise ValueError("T > lamda_cota.")
+    if len(interv) != len(lamda_cota):
+        raise ValueError("Faltan lambdas cotas o intervalos.")
+    if interv != sorted(interv):
+        raise ValueError("La lista de intervalos debe estar ordenada.")
+
+    j = 0  # recorre subintervalos.
+
+    t = -math.log(1 - random.random()) / lamda_cota[j]
+    eventos = []
+
+    while t <= T:
+        if t <= interv[j]:
+            V = random.random()
+            if V < (lamda_t(t) / lamda_cota[j]):
+                eventos.append(t)
+
+            t += -math.log(1 - random.random()) / lamda_cota[j]
+
+        else:  # t > interv[j]
+            t = interv[j] + (t - interv[j]) * lamda_cota[j] / lamda_cota[j + 1]
+            j += 1
+
+    return eventos
 
 
 def exponencial_2(lamda: float):
